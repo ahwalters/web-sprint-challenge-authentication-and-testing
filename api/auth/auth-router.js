fda445ce4660969db02ef44a001977bf03ed20f6
Alana Walters <alana.walters@yahoo.com>
2023-05-03 16:36:05 -0400

    created login auth router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -52,31 +52,64 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+// router.post('/login', (req, res) => {
+//   res.end('implement login, please!');
+//   /*
+//     IMPLEMENT
+//     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
+//     1- In order to log into an existing account the client must provide `username` and `password`:
+//       {
+//         "username": "Captain Marvel",
+//         "password": "foobar"
+//       }
 
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
+//     2- On SUCCESSFUL login,
+//       the response body should have `message` and `token`:
+//       {
+//         "message": "welcome, Captain Marvel",
+//         "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+//       }
 
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+//     3- On FAILED login due to `username` or `password` missing from the request body,
+//       the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+//     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+//       the response body should include a string exactly as follows: "invalid credentials".
+//   */
+// });
+
+router.post('/login', async (req, res) => {
+      const {username, password} = req.body
+      try {
+        if (!username || !password) {
+          res.status(400).json({message : "username and password required"})
+        } else {
+          const [user] = await db('users').where('username', username)
+          if (user && bycrpt.compareSync(password, user.password)) {
+            req.session.user = user;
+            const token = generateToken(user);
+            res.json({
+              message : `welcome, ${username}`,
+              token
+            })
+          }
+        }
+      } catch (err){
+          res.json('invalid credentials');
+        }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  const secret = 'test'
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
